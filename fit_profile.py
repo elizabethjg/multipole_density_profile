1c3e49433ad7516c56dcd0d1c71a0a0d54559748
eli <elizabeth.johana.gonzalez@gmail.com>
2019-11-13 10:06:10 -0300

    adding fitting code

---
+++ fit_profile.py
@@ -0,0 +1,58 @@
+import numpy as np
+from pylab import *
+from astropy.cosmology import LambdaCDM
+from scipy.misc import derivative
+from scipy import integrate
+from multipoles_shear import *
+from scipy.optimize import minimize
+
+np.random.seed(123)
+
+# Choose the "true" parameters.
+m_true = -0.9594
+b_true = 4.294
+f_true = 0.534
+
+# Generate some synthetic data from the model.
+N = 50
+x = np.sort(10 * np.random.rand(N))
+yerr = 0.1 + 0.5 * np.random.rand(N)
+y = m_true * x + b_true
+y += np.abs(f_true * y) * np.random.randn(N)
+y += yerr * np.random.randn(N)
+
+plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+x0 = np.linspace(0, 10, 500)
+plt.plot(x0, m_true * x0 + b_true, "k", alpha=0.3, lw=3)
+plt.xlim(0, 10)
+plt.xlabel("x")
+plt.ylabel("y")
+
+def log_likelihood(theta, x, y, yerr):
+    m, b, log_f = theta
+    model = m * x + b
+    sigma2 = yerr ** 2 + model ** 2 * np.exp(2 * log_f)
+    return -0.5 * np.sum((y - model) ** 2 / sigma2 + np.log(sigma2))
+    
+np.random.seed(42)
+nll = lambda *args: -log_likelihood(*args)
+initial = np.array([m_true, b_true, np.log(f_true)]) + 0.1 * np.random.randn(3)
+soln = minimize(nll, initial, args=(x, y, yerr))
+m_ml, b_ml, log_f_ml = soln.x
+
+print("Maximum likelihood estimates:")
+print("m = {0:.3f}".format(m_ml))
+print("b = {0:.3f}".format(b_ml))
+print("f = {0:.3f}".format(np.exp(log_f_ml)))
+
+plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+plt.plot(x0, m_true * x0 + b_true, "k", alpha=0.3, lw=3, label="truth")
+plt.plot(x0, np.dot(np.vander(x0, 2), w), "--k", label="LS")
+plt.plot(x0, np.dot(np.vander(x0, 2), [m_ml, b_ml]), ":k", label="ML")
+plt.legend(fontsize=14)
+plt.xlim(0, 10)
+plt.xlabel("x")
+plt.ylabel("y")
+
+
+print 'END OF PROGRAM'
--- plotting_profiles.py
+++ plotting_profiles.py
@@ -30,9 +30,12 @@ for q in np.arange(0.1,1.,0.2):
     ax1.plot(r,gt0,'C0',label = 'Only monopole',alpha = 1.-(q/2.))
     ax1.plot(r,gt,'C2',label = 'Monopole + misscentred (e=0)',alpha = 1.-(q/2.))
     ax1.plot(r,gt_e,'C1',label = 'Monopole + misscentred (e=0.25)',alpha = 1.-(q/2.))
+    ax1.plot(r,(1-pcc)*gt_off,'C1--',label = 'Monopole + misscentred (e=0.25)',alpha = 1.-(q/2.))
     ax1.set_xscale('log')
+    ax1.set_yscale('log')
     ax1.set_ylabel(r'$\Gamma_t0$')
     ax1.set_xlabel('r [Mpc]')
+    ax1.axis([0,1.5,1,350.])    
     # ax1.legend()
 
     
@@ -44,23 +47,93 @@ for q in np.arange(0.1,1.,0.2):
     ax2.set_title('Gamma_t_cos(2theta)',alpha = 1.-(q/2.))
     ax2.plot(r,gt2,'C0',label = 'Only quadrupole',alpha = 1.-(q/2.))
     ax2.plot(r,gt_cos,'C1',label = 'quadrupole + misscentred',alpha = 1.-(q/2.))
+    ax2.plot(r,(1-pcc)*gt_cos_off,'C1--',label = 'quadrupole + misscentred',alpha = 1.-(q/2.))
     ax2.set_xscale('log')
+    ax2.set_yscale('log')
     ax2.set_ylabel(r'$\Gamma_t2$')
     ax2.set_xlabel('r [Mpc]')
+    ax2.axis([0,1.5,1,350.])
     # ax2.legend()
     
     
     gx_sin = pcc*gx2 + (1-pcc)*gx_sin_off
     
 
-    ax3.set_title('Gamma_x_cos(2theta)',alpha = 1.-(q/2.))
+    ax3.set_title('Gamma_x_sin(2theta)',alpha = 1.-(q/2.))
     ax3.plot(r,gx2,'C0',label = 'Only quadrupole',alpha = 1.-(q/2.))
     ax3.plot(r,gx_sin,'C1',label = 'quadrupole + misscentred',alpha = 1.-(q/2.))
+    ax3.plot(r,(1-pcc)*gx_sin_off,'C1--',label = 'quadrupole + misscentred',alpha = 1.-(q/2.))
     ax3.set_xscale('log')
     ax3.set_ylabel(r'$\Gamma_x2$')
     ax3.set_xlabel('r [Mpc]')
+    ax3.axis([0,1.5,-50.,30.])
     # ax3.legend()
 
+f, ax1 = plt.subplots()
+f, ax2 = plt.subplots()
+f, ax3 = plt.subplots()
+q = 0.
 
+for logM in np.arange(12.5,14,0.5):
+
+    out = np.loadtxt('multipoles_'+str(logM)+'.out')
+    
+    r = out[:,0]
+    gt0 = out[:,1]
+    gt2 = out[:,2]
+    gx2 = out[:,3]
+    
+    gt0_off = out[:,4]
+    gt_off  = out[:,5]
+    gt_cos_off = out[:,6]
+    gx_sin_off = out[:,7]
+    
+    pcc = 0.8
+    
+    gt = pcc*gt0 + (1-pcc)*gt0_off
+    gt_e = pcc*gt0 + (1-pcc)*gt_off
+    
+
+    ax1.set_title('Gamma_t')
+    ax1.plot(r,gt0,'C0',label = 'Only monopole',alpha = 1.-(q/2.))
+    ax1.plot(r,gt,'C2',label = 'Monopole + misscentred (e=0)',alpha = 1.-(q/2.))
+    ax1.plot(r,gt_e,'C1',label = 'Monopole + misscentred (e=0.25)',alpha = 1.-(q/2.))
+    ax1.plot(r,(1-pcc)*gt_off,'C1--',label = 'Monopole + misscentred (e=0.25)',alpha = 1.-(q/2.))
+    ax1.set_xscale('log')
+    ax1.set_yscale('log')
+    ax1.set_ylabel(r'$\Gamma_t0$')
+    ax1.set_xlabel('r [Mpc]')
+    ax1.axis([0,1.5,1,350.])    
+    # ax1.legend()
+
+    
+    # FOR ellip = 0.25
+    
+    gt_cos = pcc*gt2 + (1-pcc)*gt_cos_off
+    
+
+    ax2.set_title('Gamma_t_cos(2theta)',alpha = 1.-(q/2.))
+    ax2.plot(r,gt2,'C0',label = 'Only quadrupole',alpha = 1.-(q/2.))
+    ax2.plot(r,gt_cos,'C1',label = 'quadrupole + misscentred',alpha = 1.-(q/2.))
+    ax2.plot(r,(1-pcc)*gt_cos_off,'C1--',label = 'quadrupole + misscentred',alpha = 1.-(q/2.))
+    ax2.set_xscale('log')
+    ax2.set_yscale('log')
+    ax2.set_ylabel(r'$\Gamma_t2$')
+    ax2.set_xlabel('r [Mpc]')
+    ax2.axis([0,1.5,1,350.])
+    # ax2.legend()
+    
+    
+    gx_sin = pcc*gx2 + (1-pcc)*gx_sin_off
+    
+
+    ax3.set_title('Gamma_x_sin(2theta)',alpha = 1.-(q/2.))
+    ax3.plot(r,gx2,'C0',label = 'Only quadrupole',alpha = 1.-(q/2.))
+    ax3.plot(r,gx_sin,'C1',label = 'quadrupole + misscentred',alpha = 1.-(q/2.))
+    ax3.plot(r,(1-pcc)*gx_sin_off,'C1--',label = 'quadrupole + misscentred',alpha = 1.-(q/2.))
+    ax3.set_xscale('log')
+    ax3.set_ylabel(r'$\Gamma_x2$')
+    ax3.set_xlabel('r [Mpc]')
+    ax3.axis([0,1.5,-50.,30.])
 
 print 'END OF PROGRAM'
