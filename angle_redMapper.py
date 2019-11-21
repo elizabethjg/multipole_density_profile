983b3f099a033906e6b11f5f158827fc4f5c52bc
eli <elizabeth.johana.gonzalez@gmail.com>
2019-11-21 17:42:44 -0300

    rotate redMapper

--- angle_redMapper.py
+++ angle_redMapper.py
@@ -2,6 +2,8 @@ import numpy as np
 # from pylab import *
 from astropy.io import fits
 #parameters
+from astropy.cosmology import LambdaCDM
+cosmo = LambdaCDM(H0=70., Om0=0.3, Ode0=0.7)
 
 folder = '/home/eli/Documentos/Astronomia/posdoc/halo-elongation/redMapper/'
 
@@ -9,12 +11,22 @@ members  = fits.open(folder+'redmapper_dr8_public_v6.3_members.fits')
 clusters = fits.open(folder+'redmapper_dr8_public_v6.3_catalog.fits')
 
 ides  = members[1].data['ID']
+ID,c = np.unique(ides,return_counts=True)
+
 R_cen = members[1].data['R']
 RA    = members[1].data['RA']
 DEC   = members[1].data['DEC']
 
 P     = members[1].data['P']
-Lum_r = 10.**(-0.4*members[1].data['MODEL_MAG_R'])
+zspec = clusters[1].data['Z_SPEC']
+zlambda = clusters[1].data['Z_LAMBDA']
+zc = zspec
+zc[zc<0] = zlambda[zc<0]
+zc = np.repeat(zc,c)
+D_lum    = np.array(cosmo.luminosity_distance(zc))*1.e6
+MAG_abs=members[1].data['MODEL_MAG_R']+5.0-5.0*np.log10(D_lum)
+
+Lum_r = 10.**(-0.4*MAG_abs)
 
 ID_c  = clusters[1].data['ID']
 
@@ -35,15 +47,20 @@ theta_pcut = np.array([])
 e_pcutl = np.array([])
 theta_pcutl = np.array([])
 
+e_pcutd = np.array([])
+theta_pcutd = np.array([])
+
 plots = np.linspace(1,len(ID_c),10)
 
 # for j in plots.astype(int):
+
 for j in range(len(ID_c)):
      print j
      mid  = ides == ID_c[j]
      N = np.append(N,mid.sum())
      ra   = RA[mid]
      dec  = DEC[mid]
+     
      # all members
      mcen = R_cen[mid] == 0
      ra0  = ra[mcen]
@@ -55,6 +72,7 @@ for j in range(len(ID_c)):
      E2 = (2.*Q12)/(Q11+Q22)
      e = np.append(e,np.sqrt(E1**2 + E2**2))
      theta = np.append(theta,np.arctan2(E2,E1)/2.)
+     
      # weighted by luminosity
      wl    = Lum_r[mid]
      mcen = R_cen[mid] == 0
@@ -67,6 +85,7 @@ for j in range(len(ID_c)):
      E2 = (2.*Q12)/(Q11+Q22)
      e_lum = np.append(e_lum,np.sqrt(E1**2 + E2**2))
      theta_lum = np.append(theta_lum,np.arctan2(E2,E1)/2.)
+     
      # weighted by p_member
      wc   = P[mid]
      mcen = R_cen[mid] == 0
@@ -79,6 +98,7 @@ for j in range(len(ID_c)):
      E2 = (2.*Q12)/(Q11+Q22)
      e_p = np.append(e_p,np.sqrt(E1**2 + E2**2))
      theta_p = np.append(theta_p,np.arctan2(E2,E1)/2.)
+     
      # p_cut
      pcut = P[mid] > 0.5
      mcen = R_cen[mid] == 0
@@ -91,6 +111,7 @@ for j in range(len(ID_c)):
      E2 = (2.*Q12)/(Q11+Q22)
      e_pcut = np.append(e_pcut,np.sqrt(E1**2 + E2**2))
      theta_pcut = np.append(theta_pcut,np.arctan2(E2,E1)/2.)
+     
      # p_cut weighted by lum
      pcut = P[mid] > 0.5
      mcen = R_cen[mid] == 0
@@ -103,6 +124,21 @@ for j in range(len(ID_c)):
      E2 = (2.*Q12)/(Q11+Q22)
      e_pcutl = np.append(e_pcutl,np.sqrt(E1**2 + E2**2))
      theta_pcutl = np.append(theta_pcutl,np.arctan2(E2,E1)/2.)    
+     
+     # p_cut weighted by distance
+     wd   = 1./((ra - ra0)**2 + (dec -dec0)**2)
+     pcut = P[mid] > 0.5
+     mcen = R_cen[mid] == 0
+     ra0  = ra[mcen]
+     dec0 = dec[mcen]
+     Q11  = np.sum((ra[(~mcen)*pcut]-ra0)**2*wd[(~mcen)*pcut])/np.sum(wd[(~mcen)*pcut])
+     Q22  = np.sum((dec[(~mcen)*pcut]-dec0)**2*wd[(~mcen)*pcut])/np.sum(wd[(~mcen)*pcut])
+     Q12  = np.sum((ra[(~mcen)*pcut]-ra0)*(dec[~mcen*pcut]-dec0)*wd[(~mcen)*pcut])/np.sum(wd[(~mcen)*pcut])
+     E1 = (Q11-Q22)/(Q11+Q22)
+     E2 = (2.*Q12)/(Q11+Q22)
+     e_pcutd = np.append(e_pcutd,np.sqrt(E1**2 + E2**2))
+     theta_pcutd = np.append(theta_pcutd,np.arctan2(E2,E1)/2.)         
+     
      ''' 
      l = 0.5*(max(ra)-min(ra))
      plt.figure()
@@ -128,7 +164,9 @@ tbhdu = fits.BinTableHDU.from_columns(
         fits.Column(name='e_pcut', format='D', array=e_pcut),
         fits.Column(name='theta_pcut', format='D', array=theta_pcut),
         fits.Column(name='e_pcut_wlum', format='D', array=e_pcutl),
-        fits.Column(name='theta_pcut_wlum', format='D', array=theta_pcutl)])
+        fits.Column(name='theta_pcut_wlum', format='D', array=theta_pcutl),
+        fits.Column(name='e_pcut_wd', format='D', array=e_pcutd),
+        fits.Column(name='theta_pcut_wd', format='D', array=theta_pcutd)])
         
 tbhdu.writeto(folder+'angles_redMapper.fits',overwrite=True)        
 '''
--- angle_redMapper_analysis.py
+++ angle_redMapper_analysis.py
@@ -19,7 +19,7 @@ RA    = members[1].data['RA']#[mask]
 RA[RA > 275] = RA[RA>275] - 360.
 DEC   = members[1].data['DEC']#[mask]
 P     = members[1].data['P']
-Lum_r = 10.**(-0.4*members[1].data['MODEL_MAG_R'])
+
 
 ID,c = np.unique(ides,return_counts=True)
 
@@ -35,6 +35,10 @@ D_ang    = np.array(cosmo.angular_diameter_distance(zc))
 kpcscale = D_ang*(((1.0/3600.0)*np.pi)/180.0)*1000.0
 KPCSCALE = np.repeat(kpcscale,c)
 
+zc = np.repeat(zc,c)
+D_lum    = np.array(cosmo.luminosity_distance(zc))*1.e6
+MAG_abs=members[1].data['MODEL_MAG_R']+5.0-5.0*np.log10(D_lum)
+Lum_r = 10.**(-0.4*MAG_abs)
 
 
 mcen = R_cen == 0.
@@ -62,18 +66,20 @@ twl  = np.repeat(angles['theta_wlum'],c)
 twp  = np.repeat(angles['theta_wp'],c)
 tp   = np.repeat(angles['theta_pcut'],c)
 tpwl = np.repeat(angles['theta_pcut_wlum'],c)
+tpwd = np.repeat(angles['theta_pcut_wd'],c)
 
 e    = np.repeat(angles['e'],c)
 ewl  = np.repeat(angles['e_wlum'],c)
 ewp  = np.repeat(angles['e_wp'],c)
 ep   = np.repeat(angles['e_pcut'],c)
 epwl = np.repeat(angles['e_pcut_wlum'],c)
+epwd = np.repeat(angles['e_pcut_wd'],c)
 # '''
 
 dx = ((RA - RA0)*3600.*KPCSCALE*1.e-3)/R_lambda
 dy = ((DEC - DEC0)*3600.*KPCSCALE*1.e-3)/R_lambda
 
-mask = (abs(dx) < 0.01)*(abs(dy) < 0.01)
+mask = np.isfinite(dx)
 dx = dx[mask]
 dy = dy[mask]
 
@@ -85,6 +91,8 @@ x_p  = (dx*np.cos(tp[mask]) + dy*np.sin(tp[mask]))
 y_p = (-1.*dx*np.sin(tp[mask]) + dy*np.cos(tp[mask])) 
 x_pwl  = (dx*np.cos(tpwl[mask]) + dy*np.sin(tpwl[mask])) 
 y_pwl = (-1.*dx*np.sin(tpwl[mask]) + dy*np.cos(tpwl[mask])) 
+x_pwd  = (dx*np.cos(tpwd[mask]) + dy*np.sin(tpwd[mask])) 
+y_pwd = (-1.*dx*np.sin(tpwd[mask]) + dy*np.cos(tpwd[mask])) 
 
 lgrid = 20
 Max = 1800
@@ -95,12 +103,50 @@ ax[0,1].hexbin(x_t,y_t,gridsize=lgrid,extent=(-0.4,0.4,-0.4,0.4),vmax=Max,cmap='
 ax[0,2].hexbin(x_l,y_l,gridsize=lgrid,extent=(-0.4,0.4,-0.4,0.4),vmax=Max,cmap='pink')
 ax[1,0].hexbin(x_p,y_p,gridsize=lgrid,extent=(-0.4,0.4,-0.4,0.4),vmax=Max,cmap='pink')
 ax[1,1].hexbin(x_pwl,y_pwl,gridsize=lgrid,extent=(-0.4,0.4,-0.4,0.4),vmax=Max,cmap='pink')
+ax[1,2].hexbin(x_pwd,y_pwd,gridsize=lgrid,extent=(-0.4,0.4,-0.4,0.4),vmax=Max,cmap='pink')
 f.subplots_adjust(hspace=0,wspace=0)
 
 xedges = np.linspace(-0.4,0.4,20)
 yedges = np.linspace(-0.4,0.4,20)
+
+xedges = np.linspace(-1.,1.,20)
+yedges = np.linspace(-1.,1.,20)
 xcenters = (xedges[:-1] + xedges[1:]) / 2.
 ycenters = (yedges[:-1] + yedges[1:]) / 2.
-H, xedges, yedges = np.histogram2d(x_t, y_t, bins=(xedges, yedges))
 X,Y = np.meshgrid(xcenters,ycenters)
-cp = plt.contour(X, Y, H,100)
+H, xedges, yedges = np.histogram2d(dx, dy, bins=(xedges, yedges))
+levels = np.linspace(H.min(),10000.,7)
+
+f, ax = plt.subplots(2, 3, sharex=True, sharey=True,figsize=(10,10))
+H, xedges, yedges = np.histogram2d(dx, dy, bins=(xedges, yedges))
+ax[0,0].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_t,y_t, bins=(xedges, yedges))
+ax[0,1].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_l,y_l, bins=(xedges, yedges))
+ax[0,2].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_p,y_p, bins=(xedges, yedges))
+ax[1,0].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_pwl,y_pwl, bins=(xedges, yedges))
+ax[1,1].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_pwd,y_pwd, bins=(xedges, yedges))
+ax[1,2].contour(X, Y, H.T,levels)
+
+f.subplots_adjust(hspace=0,wspace=0)
+
+levels = np.linspace(15000,150000.,7)
+f, ax = plt.subplots(2, 3, sharex=True, sharey=True,figsize=(10,10))
+H, xedges, yedges = np.histogram2d(dx, dy, bins=(xedges, yedges),weights=np.log10(Lum_r))
+ax[0,0].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_t,y_t, bins=(xedges, yedges),weights=np.log10(Lum_r))
+ax[0,1].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_l,y_l, bins=(xedges, yedges),weights=np.log10(Lum_r))
+ax[0,2].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_p,y_p, bins=(xedges, yedges),weights=np.log10(Lum_r))
+ax[1,0].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_pwl,y_pwl, bins=(xedges, yedges),weights=np.log10(Lum_r))
+ax[1,1].contour(X, Y, H.T,levels)
+H, xedges, yedges = np.histogram2d(x_pwd,y_pwd, bins=(xedges, yedges),weights=np.log10(Lum_r))
+ax[1,2].contour(X, Y, H.T,levels)
+
+f.subplots_adjust(hspace=0,wspace=0)
+
