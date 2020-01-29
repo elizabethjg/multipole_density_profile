dbb198bd82010a100d829588f0048b3e6687c927
eli <elizabeth.johana.gonzalez@gmail.com>
2020-01-29 15:04:28 -0300

    make profile redMapper parallelized

--- run_makeprofile.py
+++ run_makeprofile.py
@@ -21,7 +21,7 @@ makeprofile_parallel(samples,lmin,lmax,zmin,zmax,z_back,odds_min,RIN,ROUT,ndots)
 '''
 
 name_cat = ['gx_CFHT_redMapper.fits']*4 + ['gx_CS82_redMapper.fits']*4 + ['gx_KiDS_redMapper.fits']*4
-samples  = ['original_bin1','original_bin2','original_bin3','original_bin4']*4
+samples  = ['original_bin1','original_bin2','original_bin3','original_bin4']*3
 lmin     = np.tile(np.array([20.,23.42,28.3,39.7]),3)
 lmax     = np.tile(np.array([23.42,28.3,39.7,145.]),3)
 zmin     = np.ones(len(lmin))*0.1
@@ -35,16 +35,16 @@ zlim     = np.append(np.ones(8)*1.3,np.ones(4)*0.9)
 
 
 name_cat = name_cat + ['gx_CFHT_redMapper.fits']*4 + ['gx_CS82_redMapper.fits']*4 + ['gx_KiDS_redMapper.fits']*4
-samples  = samples +  ['terciles_total','terciles_bin1','terciles_bin2','terciles_bin3']*4
+samples  = samples +  ['terciles_total','terciles_bin1','terciles_bin2','terciles_bin3']*3
 lmin     = np.append(lmin,np.tile(np.array([20.,20.,28.,40.]),3))
 lmax     = np.append(lmax,np.tile(np.array([150.,28.,40.,150.]),3))
-zmin     = np.append(zmin,np.ones(len(lmin))*0.1)
-zmax     = np.append(zmax,np.ones(len(lmin))*0.33)
-z_back   = np.append(z_back,np.ones(len(lmin))*0.1)
-odds_min = np.append(odds_min,np.ones(len(lmin))*0.5)
-RIN      = np.append(RIN,np.ones(len(lmin))*200.)
-ROUT     = np.append(ROUT,np.ones(len(lmin))*5000.)
-ndots    = np.append(ndots,np.ones(len(lmin))*10)
+zmin     = np.append(zmin,np.ones(12)*0.1)
+zmax     = np.append(zmax,np.ones(12)*0.33)
+z_back   = np.append(z_back,np.ones(12)*0.1)
+odds_min = np.append(odds_min,np.ones(12)*0.5)
+RIN      = np.append(RIN,np.ones(12)*200.)
+ROUT     = np.append(ROUT,np.ones(12)*5000.)
+ndots    = np.append(ndots,np.ones(12)*10)
 zlim     = np.append(zlim,np.append(np.ones(8)*1.3,np.ones(4)*0.9))
 
 makeindprofile_parallel(name_cat,samples,lmin,lmax,zmin,zmax,z_back,odds_min,RIN,ROUT,ndots,zlim)
