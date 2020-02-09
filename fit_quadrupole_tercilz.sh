b6d5ea6013ee9f9decd234f76caef6ead4f9636a
eli <elizabeth.johana.gonzalez@gmail.com>
2020-02-09 18:29:39 -0300

    ready

--- fit_quadrupole_tercilz.sh
+++ fit_quadrupole_tercilz.sh
@@ -1,143 +1,215 @@
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'both'
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z1.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'both'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'tcos'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'xsin'
+##
+##python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+##-file 'profile_terciles_z2.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'xsin'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'both'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'both'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'both'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'both'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'both'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'both'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'both'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'both'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'both'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'both'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'both'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'both'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'both'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'both'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'tcos'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'both'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'tcos'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'tcos'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'tcos'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'tcos'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'tcos'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'tcos'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'tcos'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'tcos'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'tcos'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'tcos'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'tcos'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'tcos'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'tcos'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'tcos'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'xsin'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'tcos'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'xsin'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'xsin'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'xsin'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'xsin'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'xsin'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'xsin'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'xsin'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'xsin'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'xsin'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'xsin'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'xsin'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'xsin'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z1.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'xsin'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'xsin'
 
 python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'both'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'both'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'both'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'both'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'both'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'both'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'both'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'both'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'tcos'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'tcos'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'tcos'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'tcos'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'tcos'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'tcos'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'tcos'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'tcos'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'xsin'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'xsin'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'xsin'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'xsin'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'xsin'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'xsin'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'xsin'
-
-python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
--file 'profile_terciles_z2.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'xsin'
+-file 'profile_terciles_z3.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'xsin'
