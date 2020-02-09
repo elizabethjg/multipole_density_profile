f71a4db5f2400e658c039eed2ab8df503fecb474
eli <elizabeth.johana.gonzalez@gmail.com>
2020-02-09 11:48:17 -0300

    fit quadrupole

---
+++ fit_quadrupole_median1.sh
@@ -0,0 +1,143 @@
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin1.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'both'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'tcos'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 't' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'twl' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'twd' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tp' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tpwl' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tpwd' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'tpwdl' -misscentred 'False' -component 'xsin'
+
+python -u fit_profile_quadrupole.py -folder '/mnt/clemente/lensing/redMaPPer/' \
+-file 'profile_median_bin2.cat' -ncores 10 -ang 'control' -misscentred 'False' -component 'xsin'
