d3d5aceca28c5d36e4953448f4dad67f01894b5f
eli <elizabeth.johana.gonzalez@gmail.com>
2020-03-27 18:48:51 -0300

    run test

--- run_fit.py
+++ run_fit.py
@@ -15,7 +15,7 @@ fini = int(fini)
 
 folder    = args.folder
 
-for j in range(ini,fini):
+for j in ind[m]:
     
     pname  = 'profile_medianas_'+str(j)+'.cat'
     
@@ -26,17 +26,6 @@ for j in range(ini,fini):
     # correr = 'python -u fit_profile_monopole_misscentred.py -folder '+folder+' -file '+pname+' -ncores 10'
     # os.system(correr)
 
-    f = open(folder+pname,'r')
-    lines = f.readlines()
-    pcc = float((lines[-1][1:-2]))
-    M200 = float((lines[-2][1:-2]))
-    f.close()
-    
-    f=open(folder+pname,'a')
-    f.write('#'+str('%.2f' % (10**(M200)/1.e14))+'   \n')
-    f.write('#'+str('%.2f' % (pcc))+'   \n')
-    f.close()
-    
     # os.system('mv /mnt/clemente/lensing/redMaPPer/test/profile_medianas_tp_'+str(j)+'.cat /mnt/clemente/lensing/redMaPPer/test/profile_medianas_'+str(j)+'_tp.cat')
     correr2 = 'python -u fit_profile_quadrupole.py -folder '+folder+' -file  '+pname+' -ncores 10 -ang \'tp\' -misscentred \'False\' -nit 200 -ROUT 700 -component both'
     os.system(correr2)
